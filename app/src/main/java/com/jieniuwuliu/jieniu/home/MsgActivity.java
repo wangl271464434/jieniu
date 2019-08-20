a05637c49022a6e94a27346aeacf6a7c19a8b128
wl <271464434@qq.com>
2019-08-20 20:46:22 +0800

    commit修改界面

--- app/src/main/java/com/jieniuwuliu/jieniu/home/MsgActivity.java
+++ app/src/main/java/com/jieniuwuliu/jieniu/home/MsgActivity.java
@@ -1,17 +1,23 @@
 package com.jieniuwuliu.jieniu.home;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.TextView;
 
 import com.jieniuwuliu.jieniu.R;
 import com.jieniuwuliu.jieniu.base.BaseActivity;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MsgActivity extends BaseActivity {
+public class MsgActivity extends BaseActivity implements OnLoadMoreListener, OnRefreshListener {
 
     @BindView(R.id.title)
     TextView title;
@@ -19,6 +25,8 @@ public class MsgActivity extends BaseActivity {
     TextView empty;
     @BindView(R.id.recyclerView)
     RecyclerView recyclerView;
+    @BindView(R.id.refresh)
+    SmartRefreshLayout refresh;
 
     @Override
     protected int getLayoutId() {
@@ -28,11 +36,24 @@ public class MsgActivity extends BaseActivity {
     @Override
     protected void init() {
         title.setText("消息");
+        refresh.setVisibility(View.GONE);
+        refresh.setOnRefreshListener(this);
+        refresh.setOnLoadMoreListener(this);
     }
 
 
     @OnClick(R.id.layout_back)
     public void onViewClicked() {
         finish();
+    }
+
+    @Override
+    public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+
+    }
+
+    @Override
+    public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+
     }
 }
